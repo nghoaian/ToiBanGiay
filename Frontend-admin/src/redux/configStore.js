31e504a24a50b35daea6b94d014a62893b3dd6c6
An Nguyen <nghoaian.dev@gmail.com>
2025-04-02 08:19:08 +0700

    update

--- Frontend-admin/src/redux/configStore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userReducer from "./reducer/userReducer"
-
-export const store = configureStore({
-  reducer: {
-    userReducer: userReducer
-  }
-})
-
